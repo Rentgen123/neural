42f46e2ec1fb41c635c00afabcd89909d4d61cd0
Rentgen123 <55969877+Rentgen123@users.noreply.github.com>
2020-07-13 14:59:06 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,5 +5,7 @@ nw = NeuralNetwork()
 nw.addlayer(2)
 nw.addlayer(3)
 nw.addlayer(1)
-
+nw.show_weidths()
+nw.compile()
 print(nw)
+nw.show_weidths()
--- tools.py
+++ tools.py
@@ -6,6 +6,7 @@ class NeuralNetwork():
     def __init__(self):
         self.layerslist = []
         self.layersnumber = 0
+        self.compiled = False
 
     def __str__(self):
         rep = ''
@@ -17,16 +18,37 @@ class NeuralNetwork():
         return rep
 
     def addlayer(self, neurons_amount=1):
-        layer = Layer(self.layersnumber, neurons_amount)
+        if self.layersnumber == 0:
+            layer = Layer(self.layersnumber, neurons_amount, input=True)
+        else:
+            layer = Layer(self.layersnumber, neurons_amount)
         self.layerslist.append(layer)
         self.layersnumber += 1
 
+    def compile(self):
+        self.layerslist[-1].output = True
+        for i in range(self.layersnumber-1):
+            self.layerslist[i].w = np.random.sample((self.layerslist[i+1].neurons_amount,
+             self.layerslist[i].neurons_amount))
+        self.compiled = True
+
+    def show_weidths(self):
+        if self.compiled:
+            for i in self.layerslist:
+                print(i.w)
+                print('\n')
+        else:
+            print('Not compiled yet!')
+
 class Layer():
 
-    def __init__(self, number=0, neurons_amount=1):
+    def __init__(self, number=0, neurons_amount=1,
+                input=False, output=False):
+        self.input = input
+        self.output = output
         self.number = number
         self.neurons_amount = neurons_amount
-        #self.w = np.random.sample(neurons_amount)
+        self.w = None
 
     def __str__(self):
         rep = ''
@@ -38,4 +60,8 @@ class Layer():
         rep = ''
         rep += 'Layer number:\t' + str(self.number) + '\n'
         rep += 'Neurons number:\t' + str(self.neurons_amount) + '\n'
+        if self.input:
+            rep += 'Input layer' + '\n'
+        elif self.output:
+            rep += 'Output layer' + '\n'
         return rep
