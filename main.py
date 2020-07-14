5ed694cf1b61a67e78b3efe9706a290388cdf426
Rentgen123 <55969877+Rentgen123@users.noreply.github.com>
2020-07-14 16:18:54 +0300

    Заработало!

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ from tools import *
 
 nw = NeuralNetwork()
 nw.addlayer(2)
-nw.addlayer(3)
+nw.addlayer(5)
+nw.addlayer(4)
 nw.addlayer(1)
+inputs = np.array([[0,0],[0,1],[1,0],[1,1]])
+outputs = np.array([[0],[1],[1],[0]])
+nw.train(inputs, outputs, 10000)
+for row in inputs:
+    nw.forward(row)
+    print(str(row) + '\t' + str(nw.answer))
+'''print(nw)
 nw.show_weidths()
-nw.compile()
-print(nw)
-nw.show_weidths()
+nw.show_values()'''
--- tools.py
+++ tools.py
@@ -7,6 +7,10 @@ class NeuralNetwork():
         self.layerslist = []
         self.layersnumber = 0
         self.compiled = False
+        self.alfa = 1
+        self.answer = None
+        self.error = None
+        self.etha = 0.2
 
     def __str__(self):
         rep = ''
@@ -30,6 +34,8 @@ class NeuralNetwork():
         for i in range(self.layersnumber-1):
             self.layerslist[i].w = np.random.sample((self.layerslist[i+1].neurons_amount,
              self.layerslist[i].neurons_amount))
+            self.layerslist[i].dw = np.zeros_like(self.layerslist[i].w)
+
         self.compiled = True
 
     def show_weidths(self):
@@ -40,13 +46,87 @@ class NeuralNetwork():
         else:
             print('Not compiled yet!')
 
+    def show_values(self):
+        for i in self.layerslist:
+            print('Layer\t' + str(i.number) + '\tvalues')
+            print(i.values)
+            print('\n')
+
     def forward(self, inputs):
-        if inputs.shape == self.layerslist[0].neurons_amount:
+
+        if inputs.shape == (self.layerslist[0].neurons_amount,):
             self.layerslist[0].values = inputs
         else:
             print('Length of the input values and amount of a first layers neurons must be the same!')
             return None
-        
+
+        for i in range(1, self.layersnumber):
+            self.layerslist[i].values = np.dot(self.layerslist[i-1].w,
+                                                self.layerslist[i-1].values)
+            for j in range(len(self.layerslist[i].values)):
+                self.layerslist[i].values[j] = self.fact(self.layerslist[i].values[j])
+        self.answer = self.layerslist[-1].values
+
+    def fact(self, x):
+        f = 1/(1 + np.exp(-2*self.alfa*x))
+        return f
+
+    def dfact(self, x):
+        df = 2*self.alfa*(self.fact(x) - np.power(self.fact(x),2))
+        return df
+
+    def get_error(self, outputs):
+        if self.answer != None:
+            diff = np.sum(np.power(self.answer - outputs, 2))/len(self.answer)
+        return diff
+
+    def backward(self, outputs):
+        for i in range(self.layersnumber-1,0,-1):
+            self.layerslist[i].delta = np.zeros_like(self.layerslist[i].values)
+            if i == self.layersnumber-1:
+                for j in range(0, len(self.layerslist[i].values)):
+                    self.layerslist[i].delta[j] = -2*self.alfa*self.layerslist[i].values[j]*(1 - self.layerslist[i].values[j])*(outputs[j] - self.answer[j])
+            else:
+                summ = 0
+                summ = np.dot(self.layerslist[i].w.T,
+                                self.layerslist[i+1].delta)
+                for j in range(0,len(self.layerslist[i].values)):
+                    self.layerslist[i].delta[j] = 2*self.alfa*self.layerslist[i].values[j]*(1 - self.layerslist[i].values[j])*summ[j]
+
+        for l in range(0,self.layersnumber-1):
+            ci = 0
+            for i in np.array(self.layerslist[l+1].delta, ndmin=1):
+                cj = 0
+                for j in self.layerslist[l].values:
+                    self.layerslist[l].dw[ci,cj] = -self.etha*i*j
+                    cj += 1
+                ci += 1
+            self.layerslist[l].w += self.layerslist[l].dw
+
+    def train(self, inputs, outputs, epoch_number):
+
+        if inputs.shape[0] != outputs.shape[0]:
+            print('Количество строк в inputs и outputs должно совпадать')
+            return 0
+        if inputs.shape[1] != self.layerslist[0].neurons_amount:
+            print('Количество столбцов в inputs должно совпадать с количеством нейронов в первом слое')
+            return 0
+        if outputs.shape[1] != self.layerslist[-1].neurons_amount:
+            print('Количество столбцов в outputs должно совпадать с количеством нейронов в выходном слое')
+            return 0
+
+        self.compile()
+        print('Compiled!')
+        for epoch in range(epoch_number):
+            print('Epoch number: ' + str(epoch) + '\n')
+            diff = 0
+            for rowin, rowout in zip(inputs, outputs):
+                self.forward(rowin)
+                diff = self.get_error(rowout)
+                print('Error: ' + str(diff) + '\n')
+                self.backward(rowout)
+            print('Error: ' + str(diff) + '\n')
+
 
 class Layer():
 
@@ -57,7 +137,9 @@ class Layer():
         self.number = number
         self.neurons_amount = neurons_amount
         self.w = None
-        self.values = np.zeros(neurons_amount)
+        self.values = None
+        self.delta = None
+        self.dw = None
 
     def __str__(self):
         rep = ''
